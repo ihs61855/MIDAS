a625b4a249f7eb4edfdca76e515fbff90032f101
Hansol <ihs61855>
2018-05-26 12:05:46 +0900

    User Model Change

--- MIDAS/app/src/main/java/com/midas/mobile3/midas_mobile/Model/User.java
+++ MIDAS/app/src/main/java/com/midas/mobile3/midas_mobile/Model/User.java
@@ -1,8 +1,9 @@
 package com.midas.mobile3.midas_mobile.Model;
 
 public class User {
-    String name;
-    int year;
-    String department;
+    String name; // 이름
+    int year; //나이
+    String department; // 부서
+    String rank; //직급
     int count;
 }
